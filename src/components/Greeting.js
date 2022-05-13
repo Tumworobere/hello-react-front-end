db619308b7600de7d3328477d8ed498e6f557e71
Annah Tomoro <annaht.annah@gmail.com>
2022-05-13 22:16:50 +0300

    Add setup files

---
+++ src/components/Greeting.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchmessage } from '../Redux/greeting/fetchapi';
+
+const Greeting = () => {
+  const message = useSelector((state) => state.message.message);
+  const dispatch = useDispatch();
+  const Click = () => {
+    dispatch(fetchmessage());
+  };
+
+  return (
+    <div>
+      <div>
+        Greeting:
+        {' '}
+        {message}
+      </div>
+      <button type="button" onClick={Click}>Generate new Greeting</button>
+    </div>
+  );
+};
+
+export default Greeting;
